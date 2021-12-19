4f0458a7a8695c9a120e2cf8c923cbdac8472970
gesora96 <nahidahmed9933@gmail.com>
2021-12-19 11:39:23 +0600

    cart design done

--- src/Pages/SharedComponents/TopNavigation/TopNavigation.js
+++ src/Pages/SharedComponents/TopNavigation/TopNavigation.js
@@ -58,12 +58,58 @@ const TopNavigation = ({ cart, handleShow, handleClose, show }) => {
                 )}
               </li>
               <Offcanvas show={show} onHide={handleClose} placement='end'>
-                <Offcanvas.Header closeButton>
-                  <Offcanvas.Title>Shopping Cart</Offcanvas.Title>
+                <Offcanvas.Header closeButton className='offCanvas__header'>
+                  <Offcanvas.Title>
+                    {' '}
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      strokeWidth='0'
+                      viewBox='0 0 512 512'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        fill='none'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='32'
+                        d='M320 264l-89.6 112-38.4-44.88'
+                      ></path>
+                      <path
+                        fill='none'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='32'
+                        d='M80 176a16 16 0 00-16 16v216c0 30.24 25.76 56 56 56h272c30.24 0 56-24.51 56-54.75V192a16 16 0 00-16-16zm80 0v-32a96 96 0 0196-96h0a96 96 0 0196 96v32'
+                      ></path>
+                    </svg>{' '}
+                    Shopping Cart
+                  </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                  Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images,
-                  lists, etc.
+                  <div className={styles.placeholder__text}>
+                    <span className={styles.placeholder__image}>
+                      <svg
+                        stroke='currentColor'
+                        fill='#10b981 '
+                        strokeWidth='0'
+                        viewBox='0 0 512 512'
+                        height='30px'
+                        width='30px'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path d='M454.65 169.4A31.82 31.82 0 00432 160h-64v-16a112 112 0 00-224 0v16H80a32 32 0 00-32 32v216c0 39 33 72 72 72h272a72.22 72.22 0 0050.48-20.55 69.48 69.48 0 0021.52-50.2V192a31.75 31.75 0 00-9.35-22.6zM176 144a80 80 0 01160 0v16H176zm192 96a112 112 0 01-224 0v-16a16 16 0 0132 0v16a80 80 0 00160 0v-16a16 16 0 0132 0z'></path>
+                      </svg>
+                    </span>
+                    <h6>Your cart is empty</h6>
+                    <p>No items added in your cart. Please add product to your cart list.</p>
+                  </div>
+                  <button className={styles.cart__button}>
+                    Proceed To Checkout
+                    <span>$00</span>
+                  </button>
                 </Offcanvas.Body>
               </Offcanvas>
             </ul>

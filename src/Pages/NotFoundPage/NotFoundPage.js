ead1c44383dfbaff011bada799da5d17a6ea5560
gesora96 <nahidahmed9933@gmail.com>
2021-12-20 20:45:13 +0600

    not found added

--- src/Pages/NotFoundPage/NotFoundPage.js
+++ src/Pages/NotFoundPage/NotFoundPage.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-  return <div></div>;
+  useEffect(() => {
+    document.title = 'Not Found | Mobile Store';
+  }, []);
+
+  return (
+    <Container className={styles.notFound__container}>
+      <div>
+        <h2>404</h2>
+        <h3>Page not found</h3>
+        <Link to='/'>Back to homepage</Link>
+      </div>
+    </Container>
+  );
 };
 
 export default NotFoundPage;
